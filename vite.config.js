5021594856a05c803e79c99065cee0a334df9002
Q. Omair Ahmed <me@omairahmed.me>
2025-10-04 20:21:24 -0400

    fix: add .nojekyll via public folder

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
-import { writeFileSync } from 'fs';
 
 export default defineConfig({
   base: '/markdown-pulse/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  },
-  plugins: [{
-    name: 'add-nojekyll',
-    closeBundle() {
-      writeFileSync('dist/.nojekyll', '');
-    }
-  }]
+  }
 });
